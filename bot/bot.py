0b512d754a6bceeb7561911f9aa3d3ad8c35b302
Hest <hest@DESKTOP-TR5C91G>
2025-02-02 02:34:35 +0300

    add_food bilding

--- bot/bot.py
+++ bot/bot.py
@@ -6,9 +6,11 @@ from aiogram.filters.command import Command
 from dotenv import load_dotenv
 from pathlib import Path
 import os
+import re
 
 # Включаем логирование
 logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # Подключаем env
 dotenv_path = Path(__file__).resolve().parent.parent / '.env'
@@ -40,8 +42,8 @@ async def cmd_start(message: types.Message):
 
     conn = get_db_connection()
     cursor = conn.cursor()   
-
     cursor.execute("INSERT INTO chat (user_id) VALUES (%s) ON CONFLICT DO NOTHING", (user_id,))
+    logger.info(f"User {user_id} inserted or already exists.")
     conn.commit()
     conn.close()    
 
@@ -55,6 +57,48 @@ async def cmd_help(message: types.Message):
         "/get_food - Получить список всех добавленных продуктов и частоты их кормления.\n"
     )
     await message.answer(help_text)
+
+@dp.message(Command("add_food"))
+async def cmd_add_food(message: types.Message, command):
+    # Если не переданы никакие аргументы
+    if command.args is None:
+        await message.answer("Ошибка: не переданы аргументы")
+        return
+
+    # Логируем аргументы
+    logger.info(f"Аргументы команды: {command.args}")
+
+    # Регулярное выражение для обработки аргументов
+    pattern = r"^(.*?)\s+(daily|special)$"
+    match = re.match(pattern, command.args)
+
+    # Если аргументы не соответствуют шаблону
+    if not match:
+        await message.answer("Ошибка: неверный формат аргументов. Используйте: <описание> <daily|special>")
+        return
+
+    # Извлекаем данные из регулярного выражения
+    description = match.group(1).strip()  # Описание
+    food_type = match.group(2).strip()    # Тип (daily или special)
+
+    # Логируем результат
+    logger.info(f"Описание: {description}, Тип: {food_type}")
+
+        # Если тип "special", запрашиваем интервал
+    if food_type == "special":
+        await message.answer("Раз в сколько дней давать еду?")
+        #await state.set_state(FoodStates.waiting_for_interval)  # Переводим в состояние ожидания интервала
+    else:
+        # Написать sql запросс
+
+        # Отправляем ответ пользователю
+        await message.answer(f"Еда добавленна: {description}, Тип: {food_type}")
+
+@
+
+
+
+
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
